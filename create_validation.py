682c1d2fb49e0ce7346465be41bb75c42f35ec4a
AKASH2907 <kmr2907akash@gmail.com>
2020-01-16 17:36:46 +0530

    code restyling

--- create_validation.py
+++ create_validation.py
@@ -1,6 +1,6 @@
-from os.path import isfile, join
-from os import listdir, rename, makedirs
-from shutil import copyfile, move
+from os.path import join, exists
+from os import listdir, makedirs
+from shutil import move
 import random
 
 species = [
@@ -22,41 +22,45 @@ species = [
     "wcrsrt",
 ]
 
-initial = "./train/"
-final = "./validation/"
+train_dir = "./train/"
+validation_dir = "./validation/"
 
 
 def create_validation():
+    """Validation data sepration from augmented training images.
+    Number of images chosen for validation depends upon the
+    number of images present in the directory. If less than 78,
+    then 6 images are moved into validation folder. Similarly,
+    two if conditions for cases with less than 81 and greater
+    than 85. Images are selected using random sampling.
+    """
+    for bird_specie in species:
 
-    for i in species:
+        train_imgs_path = join(train_dir, bird_specie)
+        if not exists(join(validation_dir, bird_specie)):
+            destination = makedirs(join(validation_dir, bird_specie))
 
-        source = join(initial, i)
-        destination = join(final, i)
-
-        files = listdir(source)
-        number = len(files)
+        train_imgs = listdir(train_imgs_path)
+        number = len(train_imgs)  # number of images in each category
         if number < 78:
-            samples = random.sample(files, 6)
-            for file in samples:
-                move(join(source, file), join(destination, file))
+            validation_separation = random.sample(train_imgs, 6)
+            for img_file in validation_separation:
+                move(join(train_imgs_path, img_file),
+                     join(destination, img_file))
 
         elif 78 <= number <= 81:
-            samples = random.sample(files, 8)
-            for file in samples:
-                move(join(source, file), join(destination, file))
+            validation_separation = random.sample(train_imgs, 8)
+            for img_file in validation_separation:
+                move(join(train_imgs_path, img_file),
+                     join(destination, img_file))
 
         elif number > 85:
-            samples = random.sample(files, 9)
-            for file in samples:
-                move(join(source, file), join(destination, file))
+            validation_separation = random.sample(train_imgs, 9)
+            for img_file in validation_separation:
+                move(join(train_imgs_path, img_file),
+                     join(destination, img_file))
 
 
 if __name__ == "__main__":
-
-    makedirs(final)
-
-    for i in species:
-
-        makedirs(join(final, i))
 
     create_validation()
